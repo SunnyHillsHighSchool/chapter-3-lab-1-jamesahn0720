92ec24c77b8238dcb012bc39cc602d8ee72f5ec1
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 20:43:49 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,5 +9,19 @@ import static java.lang.System.*;
 class Main {
   public static void main(String[] args) {
     //add test cases  
+     String testCase = "a b c d e f g h i";
+    StackTest test = new StackTest(testCase);
+    System.out.println("["+testCase+"]\n\n"+"popping all items from the stack\n");
+    test.popEmAll();
+
+    testCase = "1 2 3 4 5 6 7 8 9 10";
+    test.setStack(testCase);
+    System.out.println("["+testCase+"]\n\n"+"popping all items from the stack\n");
+    test.popEmAll();
+
+    testCase = "# $ % ^ * ( ) ) _";
+    test.setStack(testCase);
+    System.out.println("["+testCase+"]\n\n"+"popping all items from the stack\n");
+    test.popEmAll(); 
   }
 }
